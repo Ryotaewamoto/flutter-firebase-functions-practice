e823fe38c2129da9797f7bd6832fbf7472c9f066
ryotaewamoto <lengtaiyanben@gmail.com>
2022-08-27 23:38:00 +0900

    small fix

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,8 @@ import * as functions from 'firebase-functions'
 // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = functions.https.onRequest((request, response) => {
+  console.log('TypeScript')
+
   functions.logger.info('Hello logs!', { structuredData: true })
   response.send('Hello from Firebase!')
 })
