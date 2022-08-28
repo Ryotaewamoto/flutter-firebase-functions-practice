60141e76b53e179531faa2d32d8c6a4cf69c10b5
ryotaewamoto <lengtaiyanben@gmail.com>
2022-08-28 17:47:07 +0900

    feat: add makeUppercase function

--- functions/src/add_message.ts
+++ functions/src/add_message.ts
@@ -1,5 +1,5 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
+import * as functions from "firebase-functions"
+import * as admin from "firebase-admin"
 
 // Take the text parameter passed to this HTTP endpoint and insert it into
 // Firestore under the path /messages/:documentId/original
@@ -7,12 +7,12 @@ exports.addMessage = functions
   .region("asia-northeast1")
   .https.onRequest(async (req, res) => {
     // Grab the text parameter.
-    const original = req.query.text;
+    const original = req.query.text
     // Push the new message into Firestore using the Firebase Admin SDK.
     const writeResult = await admin
       .firestore()
       .collection("messages")
-      .add({ original: original });
+      .add({ original: original })
     // Send back a message that we've successfully written the message
-    res.json({ result: `Message with ID: ${writeResult.id} added.` });
-  });
+    res.json({ result: `Message with ID: ${writeResult.id} added.` })
+  })
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,11 @@
-import * as firebaseAdmin from "firebase-admin";
-import * as functions from "firebase-functions";
+import * as firebaseAdmin from "firebase-admin"
+import * as functions from "firebase-functions"
 // eslint-disable-next-line import/namespace
-import * as addMessage from "./add_message";
+import * as addMessage from "./add_message"
+// eslint-disable-next-line import/namespace
+import * as makeUppercase from "./make_upper_case"
 
 // Firebaseプロジェクトの初期化
-firebaseAdmin.initializeApp(functions.config().firebase);
+firebaseAdmin.initializeApp(functions.config().firebase)
 
-export { addMessage };
+export { addMessage, makeUppercase }
